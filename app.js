2f7e32c6e97aeb999d78fb02901b715dd36d5eee
buikevin96 <buikevin96@gmail.com>
2017-08-18 15:23:24 -0400

    Allows user to select desired item and the quantity. Also added an exit and incorrect response functionality

--- app.js
+++ app.js
@@ -24,5 +24,57 @@ var makeTable = function(){
 				res[i].department_name+" || "+res[i].price+" || "+
 				res[i].stock_quantity+"\n");
 		}
+	promptCustomer(res);
+	})
+}
+
+// (3:04)
+var promptCustomer = function(res) { //Takes in response object from connection.query
+	inquirer.prompt([{
+		type: 'input',
+		name: 'choice',
+		message: "What would you like to buy? [Exit with E]"
+	}]).then(function(answer){
+		var correct = false;
+
+		// If user enters in E, it will quit application
+		if(answer.choice.toUpperCase()=="E"){
+			process.exit();
+		}
+		for(var i =0; i < res.length; i++){
+			if(res[i].product_name==answer.choice){
+				correct=true;
+				var product=answer.choice;
+				var id=i;
+				inquirer.prompt({
+					type: "input",
+					name: "quant",
+					message: "How many would you like to buy?",
+					validate: function(value){
+						if(isNaN(value)==false){
+							return true;
+						} else {
+							return false;
+						}
+					}
+				}).then(function(answer){
+					if((res[id].stock_quantity-answer.quant)>0){
+						connection.query("UPDATE products SET stock_quantity='"+(res[id].stock_quantity-answer.quant)+"' WHERE product_name= '"+
+							product+"'", function(err,res2){
+								console.log("Product Purchased!");
+								makeTable();
+						})
+					} else {
+						console.log("Not a valid selection!");
+						promptCustomer(res);
+					}
+				})
+			}
+		}
+		// If user enters in a product that does not exist, it will re-run the prompt again
+		if(i==res.length && correct == false){
+			console.log("Not a valid selection!");
+			promptCustomer(res);
+		}
 	})
 }
